119724f46322fd4ad4f55db18e062c065d4de0f7
Injae Kwak <injakwak@amazon.com>
2019-04-01 22:45:36 +1100

    flask files and updated classify as module

---
+++ app.py
@@ -0,0 +1,43 @@
+import base64
+import os
+import json
+from flask import Flask, request, Response
+from classify import getModelResponse  
+
+app = Flask(__name__)
+
+def getInference(imagePath):
+    data = getModelResponse(imagePath)
+    print (data)
+    return (data)
+
+@app.route("/", methods=['GET'])
+def healthCheck():
+    return "This is a health check page"
+
+@app.route("/api/infer", methods=['POST'])
+def inference():
+    try:
+        raw_form = request.form
+        img_data = raw_form['image']
+        localPath = "/tmp/"
+        fileName = "picture.png"
+        fullPath = localPath + fileName
+
+        print (fullPath)
+        with open(fullPath, "wb") as fh:
+            fh.write(base64.b64decode(img_data))
+            print ("yay")
+
+        modelResponse = getInference(fullPath)
+
+        return Response(modelResponse, mimetype='application/json', status=200)
+    except Exception as e:
+        print ("error when decoding b64 form data and saving to /tmp")
+        print (e)
+        return Response(json.dumps({'Error': e}), mimetype='application/json', status=500)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
